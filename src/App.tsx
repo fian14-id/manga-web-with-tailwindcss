f32d2a267fa252df7f61441eee0779fb5651899b
FianEunoia <allifian051@gmail.com>
2023-03-01 21:34:08 +0700

    add minify css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import './css/main.css'
+import './css/minify.css'
 
 import Home from "./page/Home"
 import Beranda from './page/Beranda'
