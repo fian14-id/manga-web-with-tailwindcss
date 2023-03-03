5e4dba2e66eb3635927792c15b56e061ba4d2e58
FianEunoia <allifian051@gmail.com>
2023-03-03 14:28:00 +0700

    add pwa

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "fncode-MaWb";
+const urlToCache = ["index.html", "offline.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Open Cache");
+      return cache.addAll(urlToCache);
+    })
+  );
+  console.log("Installing service worker...");
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+  console.log("Activating service worker...");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+  console.log("Fetching:", event.request.url);
+});
